428e8ba0b8ee750f053375845274a2c2dd833a3a
Uminchu618 <uminchu@uminchu.jp>
2020-05-03 14:19:30 +0900

    two opt完成

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
 mod plot;
+
 use rand::prelude::*;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-const FILE_NAME: &str = "burma14.tsp";
+const FILE_NAME: &str = "att48.tsp";
 
-#[derive(Debug)]
-struct Node {
-    id: i32,
-    x: f64,
-    y: f64,
+#[derive(Debug,Clone,Copy)]
+pub struct Node {
+    pub id: i32,
+    pub x: f64,
+    pub y: f64,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut rng = rand::thread_rng();
     let mut route = read_input_file()?;
-    println!("{:?}", route);
-    println!("{:?}", total_distance(&route));
-    hillclimb(&mut route,1000000);
-    println!("{:?}", route);
-    println!("{:?}", total_distance(&route));
-    let plot_vec =  route.iter().map(|item| (item.x as f32 ,item.y as f32)).collect();
-    plot::plot_route(&plot_vec)?;
+    println!("random {:?}", total_distance(&route));
+    plot::plot_route(&route, "random.png")?;
+    nearest_neighbor(&mut route, &mut rng);
+    println!("nearest_neighbor {:?}", total_distance(&route));
+    plot::plot_route(&route, "nearest_neighbor.png")?;
+    let mut nr_route = route.clone();
+    hillclimb(&mut route, 100000, &mut rng);
+    println!("hillclimb {:?}", total_distance(&route));
+    plot::plot_route(&route, "hillclimb.png")?;
+    two_opt(&mut nr_route, 100000);
+    println!("two_opt {:?}", total_distance(&nr_route));
+    plot::plot_route(&nr_route, "two_opt.png")?;
     Ok(())
 }
 
@@ -42,18 +49,64 @@ fn total_distance(route: &Vec<Node>) -> f64 {
 /// 山登り法による経路の最適化
 ///   route: 初期経路
 ///   maxIterations: 繰り返し回数
-fn hillclimb(route: &mut Vec<Node>, max_iterations: i32) {
+fn hillclimb(route: &mut Vec<Node>, max_iterations: i32, rng: &mut rand::rngs::ThreadRng) {
     let node_count = route.len();
-    let mut rng = rand::thread_rng();
     for _current_iterations in 0..max_iterations {
         let current_total_distance = total_distance(&route);
-        let rand_index1: usize = rng.gen::<usize>()  % node_count;
+        let rand_index1: usize = rng.gen::<usize>() % node_count;
         let rand_index2: usize = rng.gen::<usize>() % node_count;
         route.swap(rand_index1, rand_index2);
         let new_total_distance = total_distance(&route);
         if new_total_distance >= current_total_distance {
             route.swap(rand_index1, rand_index2);
         }
+    }
+}
+
+fn two_opt(route: &mut Vec<Node>, max_iterations: i32) {
+    let count = route.len();
+    'outer: for _current_iterations in 0..max_iterations {
+        for i in 0..count {
+            for j in (i + 2)..count {
+                let ni = i + 1;
+                let nj = j + 1;
+                let original_dist = distance(&route[i], &route[ni % count])
+                    + distance(&route[j], &route[nj % count]);
+                let changed_dist = distance(&route[i], &route[j])
+                    + distance(&route[ni % count], &route[nj % count]);
+                if changed_dist < original_dist {
+                    // let org_total = total_distance(&route);
+                    let swap_count = ((j as f32 - ni as f32) / 2.0 + 0.5) as usize;
+                    for offset in 0..swap_count {
+                        route.swap((ni + offset) % count, (j - offset) % count);
+                    }
+                    // let new_total = total_distance(&route);
+                    // if org_total <= new_total {
+                    //     panic!("okasi");
+                    // }
+                    continue 'outer;
+                }
+            }
+        }
+        break;
+    }
+}
+
+fn nearest_neighbor(route: &mut Vec<Node>, rng: &mut rand::rngs::ThreadRng) {
+    let count = route.len();
+    let first_index = (rng.next_u32() as usize) % count;
+    route.swap(0, first_index);
+    for i in 1..count {
+        let mut min_distance: f64 = std::f64::MAX;
+        let mut min_index: usize = i;
+        for j in i..count {
+            let temp_distance = distance(&route[i - 1], &route[j]);
+            if temp_distance < min_distance {
+                min_distance = temp_distance;
+                min_index = j;
+            }
+        }
+        route.swap(i, min_index);
     }
 }
 
--- src/plot.rs
+++ src/plot.rs
@@ -1,9 +1,18 @@
+use crate::Node;
 use plotters::prelude::*;
 
-pub fn plot_route(route: &Vec<(f32,f32)>) -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new("6.png", (640, 480)).into_drawing_area();
+pub fn plot_route(route: &Vec<Node>, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new(path, (640, 480)).into_drawing_area();
     root.fill(&WHITE)?;
     let root = root.margin(10, 10, 10, 10);
+    let range_max = route
+        .iter()
+        .map(|x| (x.x as f32, x.y as f32))
+        .fold((0.0 / 0.0, 0.0 / 0.0), |m, v| (v.0.max(m.0), v.1.max(m.1)));
+    let range_min = route
+        .iter()
+        .map(|x| (x.x as f32, x.y as f32))
+        .fold((0.0 / 0.0, 0.0 / 0.0), |m, v| (v.0.min(m.0), v.1.min(m.1)));
     // After this point, we should be able to draw construct a chart context
     let mut chart = ChartBuilder::on(&root)
         // Set the caption of the chart
@@ -12,7 +21,7 @@ pub fn plot_route(route: &Vec<(f32,f32)>) -> Result<(), Box<dyn std::error::Erro
         .x_label_area_size(20)
         .y_label_area_size(40)
         // Finally attach a coordinate on the drawing area and make a chart context
-        .build_ranged(10f32..30f32, 90f32..100f32)?;
+        .build_ranged(range_min.0..range_max.0, range_min.1..range_max.1)?;
 
     // Then we can draw a mesh
     chart
@@ -26,12 +35,12 @@ pub fn plot_route(route: &Vec<(f32,f32)>) -> Result<(), Box<dyn std::error::Erro
 
     // And we can draw something in the drawing area
     chart.draw_series(LineSeries::new(
-        route.clone(),
+        route.iter().map(|node| (node.x as f32, node.y as f32)),
         &RED,
     ))?;
     // Similarly, we can draw point series
     chart.draw_series(PointSeries::of_element(
-        route.clone(),
+        route.iter().map(|node| (node.x as f32, node.y as f32)),
         5,
         &RED,
         &|c, s, st| {
